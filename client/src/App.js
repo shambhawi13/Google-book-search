19de7e9c4616dc107de9310cceee4525a3c422d8
Sham <shambhawi.kumari1393@gmail.com>
2020-08-14 14:02:51 -0700

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,31 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/navbar"
 import Footer from "./components/footer"
-import SearchBar from "./components/searchbar"
-import Jumbotron from "./components/Jumbotron"
+import Jumbotron from "./components/Jumbotron";
+import { BookProvider } from "./util/GlobalState";
+import { Home } from './pages/home';
+import { Save } from './pages/save';
+import NoMatch from "./pages/NoMatch";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar />
       <Jumbotron />
-      <SearchBar />
-      <Footer />
+      <Router>
+        <div>
+          <BookProvider>
+            <Nav />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/home" component={Home} />
+              <Route exact path="/save" component={Save} />
+              <Route component={NoMatch} />
+            </Switch>
+          </BookProvider>
+        </div>
+      </Router>
+      <Footer/>
     </div>
   );
 }
