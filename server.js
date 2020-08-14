99bceaca6a6b1e4bf7a5544e5e248f59fdbeecb3
Bart Dority <b0rgBart3@gmail.com>
2020-08-14 10:45:22 -0700

    adding data model, mongoose connections, and empty api routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +13,25 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+
+app.get("/api/books", (req,res) => {
+  // Return all saved books as JSON
+});
+
+app.post("/api/books", (req,res) => {
+  // save a new book to the database.
+});
+app.delete("/api/books", (req,res) => {
+  // delete a book from the database by Mongo `_id`.
+});
+
+app.get("/google", (req,res) => {
+  // make an api call to `https://www.googleapis.com/books/v1/volumes?q=<Book Name>` and return the relevant results.
+});
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+
 
 // Send every other request to the React app
 // Define any API routes before this runs
