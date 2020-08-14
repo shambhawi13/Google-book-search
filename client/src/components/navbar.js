e70056063d289a93f4f861709f1473e2f4013632
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-14 11:59:59 -0700

    Nodes

---
+++ client/src/components/navbar.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function Navbar(){
+    return(
+
+        <nav>
+        <div class="nav-wrapper #1976d2 blue darken-2">
+          <a href="#" class="brand-logo">Logo</a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a linkto="" >Search</a></li>
+            <li><a linkto="" >Sass</a></li>
+          </ul>
+        </div>
+      </nav>
+    );
+}
+
+export default Navbar;
