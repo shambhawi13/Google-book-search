7b002e568195c7bd22323dde8a2f71099d6f4540
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-14 13:12:07 -0700

    Adding styles

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,18 +1,23 @@
-import React from "react"
+import React from "react";
+const Styles = {
+    nav: {
+        fontWeight: 900,
+        fontSize: "26px"
+    }
+}
+function Navbar() {
+  return (
+    <nav>
+  <div class="nav-wrapper #1976d2 blue darken-2">
+    <a linkto="/" class="left" style={Styles.nav}>Books</a>
+    <ul class="right hide-on-med-and-down">
+      <li><a href="" >Search</a></li>
+      <li><a href="" >Saved</a></li>
 
-function Navbar(){
-    return(
-
-        <nav>
-        <div class="nav-wrapper #1976d2 blue darken-2">
-          <a href="#" class="brand-logo">Logo</a>
-          <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a linkto="" >Search</a></li>
-            <li><a linkto="" >Sass</a></li>
-          </ul>
-        </div>
-      </nav>
-    );
+    </ul>
+  </div>
+</nav>
+  );
 }
 
 export default Navbar;
--- client/src/components/searchbar.js
+++ client/src/components/searchbar.js
@@ -1,9 +1,40 @@
-import React from "react"
+import React from "react";
 
-function SearchBar(){
-    return (
+const Styles = {
+  search: {
+    borderRadius: "90px",
+  },
+  text:{ 
+      fontWeight: 900
+    }
 
-    );
+};
+
+function SearchBar() {
+  return (
+    <div className="container">
+      <div className="card #1565c0 blue darken-3">
+
+          <h3 className="">Search for a book</h3>
+        <nav className="white z-depth-2" style={Styles.search}>
+          <div
+            class="nav-wrapper #1976d2 blue darken-2 z-depth-0"
+            style={Styles.search}
+          >
+            <form>
+              <div class="input-field z-depth-0" style={Styles.search}>
+                <input id="search" type="search" style={Styles.search}></input>
+                <label class="label-icon" for="search">
+                  <i class="material-icons">search</i>
+                </label>
+                <i class="material-icons">close</i>
+              </div>
+            </form>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
 }
 
 export default SearchBar;
